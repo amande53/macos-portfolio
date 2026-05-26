3c0e1f3a468f1df85efdb389c62cbe453efbcff7
Amanda Anderson <amanderson9@yahoo.com>
2026-05-26 04:24:24 -0400

    implement coderabbits suggestions

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -12,7 +12,7 @@ const FONT_WEIGHTS = {
 
 const renderText = (text, className, baseWeight=400) => {
 	return [...text].map((char, i) => {
-		return <span key={`${i}-${char}`}
+		return <span key={i}
 		      className={className}
 		      style={{fontVariationSettings: `'wght' ${baseWeight}`}}>
 			{char === " "? "\u00A0": char}

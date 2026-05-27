5d684f5bb2cc14f7188665459cc32728ffa1c809
Amanda Anderson <amanderson9@yahoo.com>
2026-05-27 03:41:38 -0400

    implement windows controls and the terminal window component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { Navbar, Welcome, Dock } from '#components'
+import {Terminal} from '#windows'
 
+import { Draggable } from 'gsap/Draggable'
+import gsap from "gsap";
+
+gsap.registerPlugin(Draggable)
 
 const App = () => {
 	return (
@@ -7,6 +12,8 @@ const App = () => {
 			<Navbar />
 			<Welcome />
 			<Dock />
+			
+			<Terminal />
 		</main>
 	);
 };
